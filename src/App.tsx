f99741dd7b7c245b78c7def9bacd4262de1a9cd1
Jonathan Her <jhawj1@hotmail.com>
2024-10-14 14:18:18 -0500

    Implemented user context

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
 import Login from './pages/Login';
+import { User, UserContext } from './context/userContext';
 
 
 function App() {
+  const [user, setUser] = useState<User>();
+
   return (
     <>
-      <Header/>
-      <Routes>
-        <Route path="/login" element={<Login/>}/>
-      </Routes>
+      <UserContext.Provider value={user}>
+        <Header/>
+        <Routes>
+          <Route path="/login" element={<Login setUser={setUser}/>}/>
+        </Routes>
+      </UserContext.Provider>
     </>
   );
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,28 +1,35 @@
 import React, {useState} from "react";
-
-import LoginForm from "../../components/LoginForm";
-import fetch from "../../utilities/fetch";
 import { useNavigate } from "react-router-dom";
 
-function Login() {
+import "./Login.css";
+import LoginForm from "../../components/LoginForm";
+import fetch from "../../utilities/fetch";
+
+interface props {
+    setUser: Function,
+}
+
+function Login({setUser}: props) {
 
     const navigate = useNavigate();
     const [error, setError] = useState();
 
     async function login(username: string, password: string) {
         try {
-            const {token} = await fetch("post", "/users/login", {username, password}, {hello: "there"});
+            const {token, user} = await fetch("post", "/users/login", {username, password});
             localStorage.setItem("token", token);
+            setUser(user);
             setTimeout(() => {
                 navigate("/"); // Don't use window.location otherwise the page refreshes
             }, 3000)
         } catch (err: any) {
-            setError(err.error);
+            setError(err.error); 
         }
     }
 
     return (
         <main>
+            <h1>Log In</h1>
             <LoginForm onSubmit={login} error={error}/>
         </main>
     )
