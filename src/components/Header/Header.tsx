8d10e0e92a8e98992526dd97879095116b17caf1
Jonathan Her <jhawj1@hotmail.com>
2024-10-15 08:42:29 -0500

    Updated header to be dynamic based on logged in user

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Link } from "react-router-dom";
 
 import NavLink from "../NavLink";
 import "./Header.css";
+import { UserContext } from "../../context/userContext";
 
 function Header() {
+    const user = useContext(UserContext);
+
     return(
         <header>
             <Link to="/" id="home">
                 Some Catchy App Name or Logo
             </Link>
             <nav>
-                <NavLink to="/login">Login</NavLink>
+                {/* Todo: Change user.username to link to the profile page when done*/}
+                {!user ? <NavLink to="/login">Login</NavLink> : user.username} 
             </nav>
         </header>
     )
