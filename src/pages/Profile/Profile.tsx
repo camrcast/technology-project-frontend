1dfc11dd3a9b7436b56ceecef26b3ac3a3a288d1
camrcast <camrcast@gmail.com>
2024-10-18 08:48:31 -0700

    Don't update profile yet

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -3,14 +3,12 @@ import { useNavigate } from "react-router-dom";
 
 import "./Profile.css";
 
-interface props {
-    setUser: Function,
-}
-
 function Profile() {
-    const navigate = useNavigate();
-    const [error, setError] = useState();
-    const [displaySuccess, setDisplaySuccess] = useState(false);
+    return (
+        <main>
+            <button onClick={Profile}>Click Event</button>
+        </main>
+    )
 }
 
 export default Profile;
