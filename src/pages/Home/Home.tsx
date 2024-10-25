c7e24d6e676527cf1acf32117cb21e2bc844eba5
camrcast <camrcast@gmail.com>
2024-10-25 06:40:14 -0700

    Basic Homepage

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from "react";
+import { useParams } from "react-router-dom";
+
+import fetch from "../../utilities/fetch";
+import "./Home.css";
+import PostCard from "../../components/PostCard";
+import { Post } from "../../components/PostCard/PostCard";
+
+function Profile() {
+    const {id} = useParams();
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const getPosts = async () => {
+            try {
+                const {posts} = await fetch("get", "/posts");
+                let page = 5;
+                if (posts.length < 5){
+                    page = posts.length;
+                }
+                setPosts(posts.slice(0, page));
+            } catch {
+               // Do nothing if error
+            }
+        }
+        getPosts();
+    }, [id]);
+    return (
+        <main id="homepage">
+            <h1>WELCOME</h1>
+                <section id="posts" className="postssection">
+                    <h2>Posts</h2>
+                    {posts.length !== 0 ? 
+                        posts.map((post: Post) => {
+                            return <PostCard post={post} key={post.itemID}/>
+                        })
+                    :
+                        <p>No Posts Made</p>
+                }
+                </section>
+        </main>
+    )
+}
+
+export default Profile;
